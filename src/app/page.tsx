b13ff55a586334395cc55fae864826dfaef3bb75
VenoM9078 <roushan.venom1@gmail.com>
2023-07-18 16:32:43 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Navbar from "./components/Navbar";
 import "./Background.css";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import Editor from "@monaco-editor/react";
 import Pill from "./components/Pill";
 
@@ -41,6 +41,20 @@ function convertHtmlToJSX(html: string) {
 export default function Home() {
   const [code, setCode] = useState<string | undefined>("// Enter HTML here");
   const [jsx, setJSX] = useState("");
+  const [showNotification, setShowNotification] = useState(false);
+
+  const handleCopy = useCallback(() => {
+    navigator.clipboard
+      .writeText(jsx)
+      .then(() => {
+        console.log("Copying to clipboard was successful!");
+        setShowNotification(true);
+        setTimeout(() => setShowNotification(false), 5000); // hide notification after 5 seconds
+      })
+      .catch((err) => {
+        console.error("Could not copy text: ", err);
+      });
+  }, [jsx]);
 
   function handleEditorChange(value: string | undefined) {
     if (value !== undefined) {
@@ -74,7 +88,61 @@ export default function Home() {
               </p>
             </div>
 
-            <div className="grid grid-cols-2 gap-4 mt-16 rounded-md p-4 border bg-neutral-900 border-zinc-900 ring-1 ring-white/10 sm:mt-24 shadow-[0_20px_207px_rgba(249,_115,_22,_0.2)]">
+            {showNotification ? (
+              <div
+                aria-live="assertive"
+                className="pointer-events-none fixed z-50 inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
+              >
+                <div className="flex w-full flex-col items-center space-y-4 sm:items-end">
+                  <div className="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-zinc-900 shadow-lg ring-1 ring-black ring-opacity-5">
+                    <div className="p-4">
+                      <div className="flex items-start">
+                        <div className="flex-shrink-0">
+                          <svg
+                            className="h-6 w-6 text-green-400"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            aria-hidden="true"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                            />
+                          </svg>
+                        </div>
+                        <div className="ml-3 w-0 flex-1 pt-0.5">
+                          <p className="text-sm font-medium text-white">
+                            Successfully copied!
+                          </p>
+                        </div>
+                        <div className="ml-4 flex flex-shrink-0">
+                          <button
+                            type="button"
+                            className="inline-flex rounded-md bg-transparent text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            onClick={() => setShowNotification(false)}
+                          >
+                            <span className="sr-only">Close</span>
+                            <svg
+                              className="h-5 w-5"
+                              viewBox="0 0 20 20"
+                              fill="currentColor"
+                              aria-hidden="true"
+                            >
+                              <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+                            </svg>
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ) : null}
+
+            <div className="grid relative max-w-full sm:grid-cols-2 gap-4 mt-16 rounded-md p-4 border bg-neutral-900 border-zinc-900 ring-1 ring-white/10 sm:mt-24 shadow-[0_20px_207px_rgba(249,_115,_22,_0.2)]">
               <Editor
                 height="60vh"
                 defaultLanguage="javascript"
@@ -93,7 +161,10 @@ export default function Home() {
               <div className="relative">
                 {" "}
                 {/* added a relative positioned div */}
-                <button className="absolute p-2 bg-zinc-900 border rounded-md border-zinc-700 top-0 right-0 z-50">
+                <button
+                  onClick={handleCopy} // Add onClick handler here
+                  className="absolute p-2 bg-zinc-900 border rounded-md border-zinc-700 top-0 right-0 z-50"
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="w-5 h-5 text-white"
